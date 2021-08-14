18c0c2b47967bfe78439031abe3ffbc64b9d107b
prajwalsdhanke <prajwalsdhanke@gmail.com>
2021-08-14 15:31:47 +0530

    adding laptop model

---
+++ RestMobile/src/main/java/com/webapp/restmobile/model/Laptop.java
@@ -0,0 +1,38 @@
+package com.webapp.restmobile.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Laptop {
+
+	private long id;
+	private String brand;
+	private String serialNumber;
+	
+	
+	
+	public Laptop(long id, String brand, String serialNumber) {
+		super();
+		this.id = id;
+		this.brand = brand;
+		this.serialNumber = serialNumber;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+}
