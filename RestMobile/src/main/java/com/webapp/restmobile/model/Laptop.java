e322695f6006a0dfe451316c30b9cf1baf8cc1ec
prajwalsdhanke <prajwalsdhanke@gmail.com>
2021-08-14 15:40:34 +0530

    final laptop

--- RestMobile/src/main/java/com/webapp/restmobile/model/Laptop.java
+++ RestMobile/src/main/java/com/webapp/restmobile/model/Laptop.java
@@ -8,35 +8,39 @@ public class Laptop {
 	private long id;
 	private String brand;
 	private String serialNumber;
-	
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Laptop [id=" + id + ", brand=" + brand + ", serialNumber=" + serialNumber + "]";
 	}
+
 	public Laptop(long id, String brand, String serialNumber) {
 		super();
 		this.id = id;
 		this.brand = brand;
 		this.serialNumber = serialNumber;
 	}
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+
 	public String getSerialNumber() {
 		return serialNumber;
 	}
+
 	public void setSerialNumber(String serialNumber) {
 		this.serialNumber = serialNumber;
 	}
